dff874c303df29032f682c41dea3a20f47f3ff29
Maximo Fernandez <maximo148601@gmail.com>
2024-04-19 00:33:38 -0300

    clase Articulo

---
+++ TP-WinForm/Clases/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm.Clases
+{
+    class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public float Precio { get; set; }
+    }
+}

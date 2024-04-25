6116c305b28abab29d7474be62168d243e51a623
Maximo Fernandez <maximo148601@gmail.com>
2024-04-25 17:17:27 -0300

    Biblioteca y clases negocio

--- negocio/ArticuloNegocio.cs
+++ negocio/ArticuloNegocio.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace dominio
+namespace negocio
 {
-    public class Class1
+    internal class ArticuloNegocio
     {
     }
 }

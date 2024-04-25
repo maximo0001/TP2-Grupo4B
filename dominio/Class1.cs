65ab5631b4a9dd4f847358d65220eef6a7da2442
Maximo Fernandez <maximo148601@gmail.com>
2024-04-25 16:47:53 -0300

    Creacion de biblioteca de clases para dominio y en teoría todas las clases que van en dominio

--- dominio/Class1.cs
+++ dominio/Class1.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TP_WinForm.Clases
+namespace dominio
 {
-    class Marca
+    public class Class1
     {
-        public int Id { get; set; }
-        public string Descripcion { get; set; }
     }
 }
--- dominio/Marca.cs
+++ dominio/Marca.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TP_WinForm.Clases
+namespace dominio
 {
-    class Categoria
+    internal class Marca
     {
-        public int Id { get; set; }
-        public string Descripcion { get; set; }
     }
 }

51d9de9e39c548732df491fbd5d88f2135592b34
Maximo Fernandez <maximo148601@gmail.com>
2024-04-28 21:01:23 -0300

    Se desvinculó cuando le cambie el nombre a ver si lo arreglé tambien agregué el listar imagenes de un articulo

---
+++ TPWinForm_Equipo-I3-main/TP-WinForm/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace TP_WinForm
+{
+    partial class frmArticulo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvArticulos.Location = new System.Drawing.Point(33, 69);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.Size = new System.Drawing.Size(523, 258);
+            this.dgvArticulos.TabIndex = 0;
+            this.dgvArticulos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // frmArticulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvArticulos);
+            this.Name = "frmArticulo";
+            this.Text = "Articulos";
+            this.Load += new System.EventHandler(this.frmArticulo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvArticulos;
+    }
+}
+
---
+++ negocio/ImagenNegocio.cs
@@ -0,0 +1,44 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class ImagenNegocio
+    {
+        public List<Categoria> Listar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            List<Categoria> lista = new List<Categoria>();
+
+            try
+            {
+                datos.setConsulta("select id, idArticulo, ImagenUrl from IMAGENES where IdArticulo = @idArt");
+                datos.setearParametro("@idArt", id);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)datos.Lector["id"];
+                    aux.Id = (int)datos.Lector["idArticulo"];
+                    aux.Descripcion = (string)datos.Lector["ImagenUrl"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
